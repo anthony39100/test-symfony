214952df2161ca5677b68d70086534b48e977262
anthony39100 <anthonybreviglieri.ab@gmail.com>
2020-01-24 08:37:07 +0000

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+
+{
+/**
+* @Route("/", name="default")
+*/ 
+
+public function default()
+{
+
+
+return new Response(
+'<html><body>Home</body></html>'
+);
+}
+}
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+   /**
+*@Route("/delete/{id}", name="projet_delete", requirements={"id"="\d+"})
+*/
+public function delete($id)
+{
+return $this->render('projet/index.html.twig', [
+'controller_name' => 'ProjetController',
+]);
+}
